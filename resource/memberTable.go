cf5842a0dcd013794ad0cc87e6736a6b08f2951a
SihanY <87457496+SihanY@users.noreply.github.com>
2022-02-08 20:56:38 +0800

    Update memberTable.go

--- resource/memberTable.go
+++ resource/memberTable.go
@@ -1,10 +1,13 @@
-// Member 成员数据表
+package resource
+
+import . "TrainingProgram/model"
+
+// Member数据表
 type Member struct {
-	ID       uint   `gorm:"primaryKey;autoIncrement"`          //ID 主键
-	Name     string `gorm:"<-;type:varchar(20)"`               //昵称
-	UserName string `gorm:"<-:create;unique;type:varchar(20)"` //用户名
-	Password string `gorm:"<-:create;type:varchar(20)"`        //密码
-	UserType uint8  `gorm:"<-:create"`                         //用户类型：1.管理员；2.学生；3.老师
-	Deleted  bool   `gorm:"<-;default:false"`                  //是否删除
-	CourseID uint   `gorm:"<-:create"`                         //外键，关联老师的课程表
+	UserID   uint64   `gorm:"primaryKey;autoIncrement"`          //ID 主键
+	Nickname string   `gorm:"<-;type:varchar(20)"`               //昵称
+	Username string   `gorm:"<-:create;unique;type:varchar(20)"` //用户名
+	Password string   `gorm:"<-:create;type:varchar(20)"`        //密码
+	UserType UserType `gorm:"<-:create"`                         //用户类型：1.管理员；2.学生；3.老师
+	Deleted  bool     `gorm:"<-;default:false"`                  //是否删除
 }
