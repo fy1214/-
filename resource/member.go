d4469ccfbab1fcddca0cbb1ed2f3b48659d78cb9
SihanY <87457496+SihanY@users.noreply.github.com>
2022-02-09 20:40:38 +0800

    Update member.go

--- resource/member.go
+++ resource/member.go
@@ -1,13 +1,19 @@
 package resource
 
-import . "TrainingProgram/model"
+type UserType int
+
+const (
+	Admin   UserType = 1
+	Student UserType = 2
+	Teacher UserType = 3
+)
 
 // Member数据表
 type Member struct {
 	UserID   uint64   `gorm:"primaryKey;autoIncrement"`          //ID 主键
 	Nickname string   `gorm:"<-;type:varchar(20)"`               //昵称
 	Username string   `gorm:"<-:create;unique;type:varchar(20)"` //用户名
-	Password string   `gorm:"<-:create;type:varchar(20)"`        //密码
+	Password string   `gorm:"<-:create;type:varchar(100)"`       //密码
 	UserType UserType `gorm:"<-:create"`                         //用户类型：1.管理员；2.学生；3.老师
 	Deleted  bool     `gorm:"<-;default:false"`                  //是否删除
 }
