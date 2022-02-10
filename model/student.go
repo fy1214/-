bf3b52df1838c29bec8729b880f1da2e455642b0
LBWyes <834264619@qq.com>
2022-02-10 15:04:05 +0800

    获取学生课表

--- model/student.go
+++ model/student.go
@@ -8,22 +8,24 @@ import (
 
 type StudentCourse struct {
 	gorm.Model
-	StudentID  uint `gorm:"index:idx_studentID;comment:'选课学生'"`
-	CoursesID  uint `gorm:"column:;comment:'学生课程'"`
-	CourseName string
-	TeacherID  uint
+	StudentID  uint   `gorm:"column:student_id;"`
+	CoursesID  uint   `gorm:"column:course_id"`
+	CourseName string `gorm:"column:course_name"`
+	TeacherID  uint   `gorm:"column:teacher_id"`
 }
 
 func GetStudentCourse(StudentID string) (*[]StudentCourse, ErrNo) {
-	var Courses []StudentCourse
 	var student Member
 	ID, _ := strconv.Atoi(StudentID)
+
 	//查找是否有对应学生
 	result := DB.First(&student, ID)
 	if result.Error != nil {
 		return nil, StudentNotExisted
 	}
+
 	//查找到学生之后得出课表
+	var Courses []StudentCourse
 	result2 := DB.Where("StudentID = ?", ID).Find(&Courses)
 	if result2.Error != nil {
 		return nil, StudentHasNoCourse
